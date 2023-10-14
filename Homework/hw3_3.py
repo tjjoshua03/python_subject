3b4d74af275ef1032e278d368bb5c37acb5842fd
Joshua <tjjoshua03@gmail.com>
2023-10-14 21:43:49 +0900

    first commit

--- Homework/hw3_3.py
+++ Homework/hw3_3.py
@@ -26,4 +26,40 @@ class Rectange(Shape):
         self.mystr = "I am a rectangle with" + str(self.length) + "and" +str(self.width)+ "as sides"
         return self.mystr
     
+class Triangle(Shape):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
     
+    def getarea(self):
+        s = (self.a + self.b + self.c) / 2
+        self.area = (s * (s - self.a) * (s - self.b) * (s - self.c))** 0.5
+        return self.area
+    
+    def getperimeter(self):
+        self.pertimeter = self.a + self.b + self.c
+        return self.pertimeter
+    
+    def __str__(self):
+        self.mystr = "I am a triangle with" + str(self.a) + "," + str(self.b) + "," + str(self.c) + "as sides"
+        return self.mystr
+    
+    
+class Circle(Shape):
+    pi = 3.14
+    
+    def __init__(self, r):
+        self.r = r
+        
+    def getarea(self):
+        self.area = pi * self.r * self.r
+        return self.area
+    
+    def getperimeter(self):
+        self.pertimeter = 2 * pi * self.r
+        return self.pertimeter
+    
+    def __str__(self):
+        self.mystr = "I am a circle with" + str(self.r) + "as radius and" + self.area + "as area and" + self.pertimeter + "as perimeter"
+        return self.mystr
